04c06270965575d13d089b6879e9030bde01cfa1
Surat <surajadkhari@gmail.com>
2023-04-03 16:02:57 +0545

    deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,8 +36,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "cbad9c58b09a52e5b7cfbf1b4a7b0312",
-"/": "cbad9c58b09a52e5b7cfbf1b4a7b0312",
+"index.html": "7435e44d23e87d0b1f233a55150902d5",
+"/": "7435e44d23e87d0b1f233a55150902d5",
 "main.dart.js": "43c50d15424a4f3aed4216db07534c0f",
 "manifest.json": "d8fe34f7ae4c072a77b924e01dac8a50",
 "version.json": "9b818ca9511483c901bed1545384376c"
